c520e611da1ff518bd70c62554283a3bc8193560
Vanessa Jesik <vajesik@gmail.com>
2023-07-16 11:40:04 -0600

    working in the DOM

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const removeMain = document.getElementById("main");
+removeMain.remove()
+
+const newHeader = document.createElement("h1")
+//document.body.append(newHeader)
+
+newHeader.id = 'victory'
+
+newHeader.textContent = 'Vanessa Jesik is the champion!'
